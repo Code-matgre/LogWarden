44b7e3cf14500b9e64a383a1d3b55a6287832c45
Code-matgre <abbracciaventomatteo@gmail.com>
2026-06-29 11:58:34 +0200

    Optimized the parsing, now with error management
    Added only a log_pattern for optimizing the parsing, fetching of the log file and there is a better error management

--- logwarden.py
+++ logwarden.py
@@ -1,23 +1,42 @@
 import re
-def read_file(file_path):
-    with open(file_path, 'r') as f:
-        for line in f:
             print(line.strip())
+import os
+
+LOG_PATTERN = re.compile(
+    r'(?P<ip>\d{1,3}(?:\.\d{1,3}){3})'       # IP
+    r'.*?'                                   # toglie i campi intermedi (identikit, user)
+    r'\"(?P<request>[^\"]+)\"'               # Estrae la richiesta (es: "GET /index.html HTTP/1.1")
+    r'\s+(?P<status>\d{3})'                  # Estrae status code (es: 200)
+)
+
 
 def parse_line(line):
-    result = {}
-    #searching matching
-    patternip = r"(\d{1,3}(?:\.\d{1,3}){3})" #ip patterns
-    matchip = re.search(patternip, line)
-
-    patternreq = r'"([^"]+)"' #requests pattern
-    matchreq = re.search(patternreq, line) 
-
-    patternstat = r'"\s+(\d{3})' #status pattern
-    matchstat = re.search(patternstat, line)
+    match = LOG_PATTERN.search(line)
+    if match:
+        return match.groupdict()
     
-    result['ip'] = matchip.group(1) if matchip else "none"
-    result['request'] = matchreq.group(1) if matchreq else "none"
-    result['status'] = matchstat.group(1) if matchstat else "none"
+    return {'ip': 'none', 'request': 'none', 'status': 'none'}
+
+def process_log_file(file_path):
+    if not os.path.exists(file_path):
+        print(f"[-] Errore: Il file '{file_path}' non esiste.")
+        return []
+
+    parsed_d = []
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    parsed_result = parse_line(line)
+                    parsed_d.append(parsed_result)
+    except Exception as e:
+        print(f"[-] Errore durante la lettura del file: {e}")
+    return parsed_d
+
+if __name__ == "__main__":
+    log_file = "Logs/mockaccess.log"
+    results = process_log_file(log_file)
     
-    return result
+    for entry in results[:5]:
+        print(entry)
